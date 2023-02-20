ee73189e01b7e4af398aaae465d6075d2833edee
Наталья Литвинова <sokolisaf@gmail.com>
2023-02-20 09:58:27 +0300

    Добавлена функция для проверки палиндрома

--- js/functions.js
+++ js/functions.js
@@ -2,4 +2,8 @@ const checkLength = (string, length) => (string.length) <= length;
 
 checkLength('проверяемая строка', 20);
 
+function isPalindrome(string) {
+  return string.split('').reverse().join('') === string;
+}
 
+isPalindrome('456654');
