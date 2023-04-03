a5e601c2980261212ed2cb962659e488daa6bc30
Keks <keks@htmlacademy.ru>
2023-04-03 15:57:07 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/data.js
@@ -0,0 +1,64 @@
+import {getRandomInteger, getRandomArrayElement, createIdGenerator} from './utils.js';
+
+const PICTURE_COUNT = 25;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const COMMENT_COUNT = 20;
+const AVATAR_COUNT = 6;
+
+const NAMES = [
+  'Иван',
+  'Мария',
+  'Артем',
+  'Наталья',
+  'Виктор',
+];
+
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const DESCRIPTIONS = [
+  'Я на море',
+  'Долгожданный отпуск',
+  'Красивый закат',
+  'Встреча с друзьями',
+  'Доброе утро',
+];
+
+const generateCommentId = createIdGenerator();
+
+const createMessage = () =>
+  Array.from({ length: getRandomInteger(1, 2)}, () =>
+    getRandomArrayElement(COMMENTS)
+  ).join(' ');
+
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement(NAMES),
+});
+
+const createPicture = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
+  comments: Array.from(
+    { length: getRandomInteger(0, COMMENT_COUNT) },
+    createComment
+  ),
+});
+
+const getPictures = () =>
+  Array.from({ length: PICTURE_COUNT }, (_, pictureIndex) =>
+    createPicture(pictureIndex + 1)
+  );
+
+export {getPictures};
